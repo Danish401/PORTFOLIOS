edca769e6e0fb9ba47cff71ed50ed446c596749d
Danish401 <125354722+Danish401@users.noreply.github.com>
2024-09-21 21:19:18 +0530

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,13 +2,17 @@ import mongoose from "mongoose";
 
 export default async function connectToDB() {
   try {
-    // if (mongoose.connections[0].readyState) return;
-    await mongoose.connect(
-      "mongodb+srv://CV:CV4010@cluster0.qpstf.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    // Check if the database is already connected
+   
+    
+    // Connect to the database
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log("Database connected successfully");
   } catch (e) {
-    console.log(e);
+    console.error("Database connection error:", e);
   }
 }
